e8ad17ec6b4f93ae2f73bb384cc4c3bf340b4cdc
ArtemShon <artshon@astp.cn.ua>
2020-05-13 13:06:10 +0300

    lab-5

---
+++ DescreteStruct/lab_5/CountingSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CountingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float time;
+            int n = 10000000;
+            Stopwatch st = new Stopwatch();
+            int[] inputIntArray = new int[n]; //{ 2, 10, 5, 6, 7, 3, 13, 4, 7, 8};
+            int[] outputIntArray = new int[n];
+            Random rand = new Random();
+            for(int i = 0; i < n; i++)
+                inputIntArray[i] = rand.Next(0,10000);
+
+            st.Start();
+            outputIntArray = CountingSort(inputIntArray);
+            st.Stop();
+
+            time = (float)st.ElapsedMilliseconds / 1000;
+            Console.WriteLine("\n---" + time);
+            /*for(int i = 0;i< outputIntArray.Length; i++)
+            {
+                Console.WriteLine(outputIntArray[i]);  
+            }*/
+
+        }
+        public static int[] CountingSort(int[] inputArray)
+        { 
+            int max = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+                if (inputArray[i] >= max) max = inputArray[i];
+
+            int[] valueArray = new int[max+1];
+            int[] outputArray = new int[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                valueArray[inputArray[i]] ++;
+            }
+            List<int> intList = new List<int>();
+            for(int i = 0;i < valueArray.Length; i++)
+            {
+                int k = valueArray[i];
+                for (int n = 0; n < k; n++)
+                {
+                    intList.Add(i);
+                }
+            }
+            outputArray = intList.ToArray();
+            return outputArray;
+  
+        }
+    }
+}
---
+++ DescreteStruct/lab_5/RadixSort/RadixSort/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace RadixSort
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Random rand = new Random();
+            Stopwatch st = new Stopwatch();
+            float time;
+            int n = 10000000;
+            int[] array = new int[n];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0,10000);
+            }
+            
+
+            Console.WriteLine("Radix Sort");
+            /*foreach (int i in array)
+                Console.Write(i+";");*/
+
+            int max = Max(array);
+            st.Start();
+            for (int exp = 1; max / exp > 0; exp *= 10)
+            {
+                CountingSort(array, array.Length, exp);
+            }
+            st.Stop();
+            time = (float)st.ElapsedMilliseconds / 1000;
+            Console.WriteLine("\n---" + time);
+
+            /*Console.WriteLine();
+            foreach (int i in array)
+                Console.Write(i+";");
+                */
+            Console.ReadLine();
+        }
+
+        public static void CountingSort(int[] array, int length, int exponent)
+        {
+            
+            int[] output = new int[length];    
+            int[] count = new int[10];
+            
+            for (int i = 0; i < length; i++)
+            {
+                count[(array[i] / exponent) % 10]++;
+            }
+
+            
+            for (int i = 1; i < 10; i++)
+            {
+                count[i] += count[i - 1];
+            }
+
+            
+            for (int i = length - 1; i >= 0; i--)
+            {
+                output[count[(array[i] / exponent) % 10] - 1] = array[i];
+                count[(array[i] / exponent) % 10]--;
+            }
+
+            
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = output[i];
+            }
+        }
+
+        public static int Max(int[] array)
+        {
+            int max = 0;
+            foreach(int i in array)
+                if (i >= max) max = i;
+            return max;
+        }
+    }
+}
